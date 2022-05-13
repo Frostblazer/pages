ee2dd9e8693107fdb8633afc2fb0041dae6a15f0
Shawn Witemyre <switemyre@comcast.net>
2022-05-13 15:57:47 -0400

    Created attribute directive and structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,8 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80',
     },
   ];
+
+  checkWindow(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
